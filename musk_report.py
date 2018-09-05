c5e92a8ff0ad72039fa190bff50497e7ef38c116
yoonhohong <nrhong@gmail.com>
2018-09-05 17:44:14 +0900

    20180831 report generation
    markdown-pdf package installed into atom editor
    convert to pdf, one by one...ooo hoo

--- musk_report.py
+++ musk_report.py
@@ -1,25 +1,25 @@
 #! /usr/bin/python
 #-*- coding: utf-8 -*-
+import csv
 
-with open("musk_result.txt", "r") as f:
-    f1 = f.read()
-    lines = f1.splitlines()
+with open("musk_20180831.csv", "r") as f:
+    f1 = csv.reader(f)
+    lines = list(f1)[1:]
     for line in lines:
-        data = line.split()
-        f2name = "20170529_{0}_{1}".format(data[2], data[1])
+        data = line
+        f2name = "{0}_{1}_{2}".format(data[0], data[2], data[1])
         with open("{0}.md".format(f2name), "w") as f2:
             f2.write('''
 ## MuSK-Ab Report
 
-Hospital:{0}  \r
-Patient name:{1}  \r
-Date of sample arrival:20{2}  \r
-Date of MusK-Ab test: 2017.05.24  \r
-Date of report: 2017.05.29  \r
+Hospital:{2}  \r
+Patient name:{0}  \r
+Date of sample arrival:{1}  \r
+Date of MusK-Ab test: {3}  \r
 
 Result:  \r
-{3}  \r
-concentration = {4} IU/mL  \r
+{4}  \r
+concentration = {5} IU/mL  \r
 
 Note: For research use only, not for use in diagnostic procedures.  \r
 If you have any question, contact at nrhong@gmail.com.  \r
@@ -28,4 +28,5 @@ HongLab  \r
 Neurology  \r
 Seoul National University College of Medicine  \r
 SNU SMG Boramae Medical Center  \r
-            '''.format(data[1], data[2], data[0], data[3], data[4]))
+            '''.format(data[0], data[1], data[2], data[3], data[4], data[5])
+            )
